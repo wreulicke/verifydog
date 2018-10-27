b02ed1e5657e87b1b94b98c73f5f92a9691547af
wreulicke <saito.masaya@classmethod.jp>
2018-10-28 01:45:17 +0900

    init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+func verify() error {
+	fmt.Println(os.Args)
+	path, err := exec.LookPath(os.Args[1])
+	if err != nil {
+		return err
+	}
+	return syscall.Exec(path, "", os.Environ())
+}
+
+func mainInternal() error {
+	app := cli.NewApp()
+	app.Name = "verifydog"
+	app.Usage = "verify diff between versions"
+	app.Action = func(c *cli.Context) error {
+		return verify()
+	}
+	return app.Run(os.Args)
+}
+
+func main() {
+	err := mainInternal()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
